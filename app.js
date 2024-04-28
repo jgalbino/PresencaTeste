ec2e6ee7f5682b288e956bf4af2b7a8680c0e4ba
João Gabriel Albino Maruska <64925552+jgalbino@users.noreply.github.com>
2024-04-28 13:47:39 -0300

    Update app.js
    Trocando constante configuração FireBase

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Configuração do Firebase
-const Config = {
+const config = {
     apiKey: "AIzaSyDayv8_M_jNUwigisneCBxu_O1eRjTO7B8",
     authDomain: "presencateste-77a47.firebaseapp.com",
     projectId: "presencateste-77a47",
@@ -10,7 +10,7 @@ const Config = {
 };
 
 // Inicializar o Firebase
-const app = firebase.initializeApp(Config);
+const app = firebase.initializeApp(config);
 const db = firebase.firestore();
 
 // Criar Aula
